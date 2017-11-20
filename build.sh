e6e2f78dd66455f812a2cc6973b7cacab0daf0c3
Thomas Robert <thomas.robert@oracle.com>
2017-11-20 11:58:56 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -t oracle/weblogic:12.2.1.3-developer --build-arg ADMIN_PASSWORD=welcome1 -f Dockerfile.developer .
+docker build -t wlsdomain:12.2.1.3 --build-arg ADMIN_PASSWORD=welcome1 -f Dockerfile.developer .
